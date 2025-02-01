603e2786ff8082d29840b797bb1ba6d27968798d
RAX <astraltechh@gmail.com>
2025-02-01 08:48:00 -0600

    GLIDER

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import axios from 'axios';
 import { fileURLToPath } from 'url';
+import { Analytics } from "@vercel/analytics/react"
 
 const app = express();
 const PORT = process.env.PORT || 9853;
