7ea4e9f368d530fc70526517d3ccd9f072d213c3
noguy64 <samtammi32@gmail.com>
2025-02-08 12:15:23 -0500

    updated bare server

--- public/static/uv/uv.config.js
+++ public/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare:'https://bare.benrogo.net',
+    bare:'http://localhost:8080',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
